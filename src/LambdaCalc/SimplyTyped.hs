f0ea32ced2a8ddf0cc1f3a97d4dfea1209757d97
Stuart Terrett <shterrett@gmail.com>
2020-07-06 16:49:49 -0400

    Add sum types to simply typed lambda calc

--- src/LambdaCalc/SimplyTyped.hs
+++ src/LambdaCalc/SimplyTyped.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TypeSynonymInstances #-}
@@ -8,8 +9,11 @@
 module LambdaCalc.SimplyTyped where
 
 import Control.Monad.Except (ExceptT, MonadError, throwError)
-import Control.Monad.Reader (ReaderT, reader, local)
+import Control.Monad.Reader (ReaderT, local, ask)
+import Control.Monad.State (StateT, gets, modify)
+import Data.Foldable (for_)
 import Data.Functor.Identity (Identity)
+import Data.List (nub)
 import Data.Maybe (listToMaybe)
 import Data.Text (Text)
 import qualified Data.Text as T
@@ -38,8 +42,12 @@ data Typ =
     | TyBool
     | TyString
     | TyInt
+    | TyUnit
     | TyPair Typ Typ
     | TyProduct [(Text, Typ)]
+    | TySum [(Text, Typ)]
+  -- ^ The list of all the variants of the sum type, with the
+  -- (single) type that it may contain
     deriving (Eq, Show)
 
 newtype DeBruijn = DeBruijn { unDeBruijn :: Int }
@@ -58,24 +66,46 @@ data Term =
     | TmMkProd [(Text, Term)]
     | TmProjProd Text Term
     | TmProjUpdate Text Term Term
+    | TmDeclareSum [(Text, Typ)]
+    | TmCall Text Term
+    -- ^ Right now, used for sum type injection. However, can be used for named
+    -- functions with declared types later
+    | TmMatchSum Term [(Text, Term)]
 
-data TypeLevel = TypeLevel
-  { context :: Context
-  } deriving (Show)
+newtype TypeEnv = TypeEnv { getTypeEnv :: [(Text, Typ)] }
+  deriving (Show)
 
-type TypeChecker = ExceptT Text (ReaderT TypeLevel Identity)
+type TypeChecker = ExceptT Text (StateT TypeEnv (ReaderT Context Identity))
 
 class WithContext m where
   addBinding :: Var -> Binding -> m a -> m a
   askContext :: m Context
 
 instance WithContext TypeChecker where
-  addBinding v b k = do
-    ctx <- reader context
-    local (\tl -> tl { context = ((v, b) : ctx) }) k
-  askContext = reader context
+  addBinding v b k =
+    local ((v, b) :) k
+  askContext = ask
 
-typeOf :: (WithContext m, MonadError Text m) => Term -> m Typ
+class (MonadError Text m) => WithTypeEnv m where
+  lookupType :: Text -> m Typ
+  addType :: Text -> Typ -> m ()
+
+instance WithTypeEnv TypeChecker where
+  lookupType name = do
+    env <- gets getTypeEnv
+    case lookup name env of
+      Just t -> pure t
+      Nothing -> throwError $ "type name " <> name <> " not found"
+  addType name typ = modify $ TypeEnv . ((name, typ) :) . getTypeEnv
+
+typeOf ::
+  forall m.
+  ( WithContext m
+  , WithTypeEnv m
+  , MonadError Text m
+  )
+  => Term
+  -> m Typ
 typeOf (TmVar _ idx) =  getTypeFromContext idx
 typeOf (TmLambda var a term) =
     addBinding var (VarBind a) $
@@ -106,17 +136,78 @@ typeOf (TmStr _) = pure TyString
 typeOf (TmInt _) = pure TyInt
 typeOf (TmPair l r) = TyPair <$> typeOf l <*> typeOf r
 typeOf (TmMkProd fs) =
-  TyProduct <$> traverse (sequence . fmap typeOf) fs
+    TyProduct <$> traverse (sequence . fmap typeOf) fs
 typeOf (TmProjProd field record) = do
-  withField field record $ \_ fTyp -> pure fTyp
+    withField field record $ \_ fTyp -> pure fTyp
 typeOf (TmProjUpdate field val record) = do
-  withField field record $ \rTyp fTyp -> do
-    vTyp <- typeOf val
-    if vTyp == fTyp
-      then pure rTyp -- immutable data structure update returns the original datastructure
-      else throwError $ "type mismatch: expected " <> tshow fTyp <> " but got " <> tshow vTyp
+    withField field record $ \rTyp fTyp -> do
+      vTyp <- typeOf val
+      if vTyp == fTyp
+        then pure rTyp -- immutable data structure update returns the original datastructure
+        else throwError $ "type mismatch: expected " <> tshow fTyp <> " but got " <> tshow vTyp
+typeOf (TmDeclareSum variants) = for_ variants bindSumType >> pure TyUnit
+  where bindSumType (name, typ) = addType name $ TyFn typ (TySum variants)
+typeOf (TmCall fn arg) = do
+    fnTyp <- lookupType fn
+    haveTyp <- typeOf arg
+    case fnTyp of
+      TyFn wantTyp returnTyp ->
+        if haveTyp == wantTyp
+          then pure returnTyp
+          else throwError $ "type mismatch: expected " <> tshow wantTyp
+                          <> "but got " <> tshow haveTyp
+                          <> "when calling " <> fn
+      _ -> throwError $ "type mismatch: attempted to call " <> fn
+                        <> " but it is not a function"
+typeOf (TmMatchSum term branches) = do
+    haveType <- typeOf term
+    case haveType of
+      TySum _ -> pure ()
+      _ -> throwError $ "Match statements are only valid with sum types. "
+                        <> tshow haveType <> " was provided"
+    bs <- nub <$> traverse (uncurry branchTypes) branches
+    case bs of
+      [(target, out)] ->
+        if target == haveType
+          then pure out
+          else throwError $ "type mismatch: expected match statement for "
+                            <> tshow haveType
+                            <> " but got " <> tshow target
+      _ -> throwError $ "All branches of a match statement should have the same type, "
+                        <> "but the following types were found"
+                        <> tshow bs
+    where
+      branchTypes ::
+        Text -- ^ name of the constructor
+        -> Term -- ^ the body of the match arm
+        -> m (Typ, Typ) -- ^ ( input (sum) type
+                        --   , output type)
+      branchTypes name body = do
+        variant <- lookupType name
+        result <- typeOf body
+        case variant of
+          TyFn input s@(TySum _) ->
+            case result of
+              TyFn input' _ ->
+                if input == input'
+                  then pure (s, result)
+                  else throwError $ " type mismatch: expected match arm to take "
+                                    <> tshow input <> " but actually takes " <> tshow input'
+              armErr ->
+                throwError $ "match arm should be function from a variant of the sum type; instead is "
+                            <> tshow armErr
+          _ -> throwError $ "match arm should be a constructor of a sum type "
+                            <> "instead got " <> tshow variant
 
-withField :: (WithContext m, MonadError Text m) => Text -> Term -> (Typ -> Typ -> m Typ) -> m Typ
+withField ::
+  ( WithContext m
+  , WithTypeEnv m
+  , MonadError Text m
+  )
+  => Text
+  -> Term
+  -> (Typ -> Typ -> m Typ)
+  -> m Typ
 withField field record k = do
   rTyp <- typeOf record
   case rTyp of
